fdeb65f0ddbb26940b002639e5023ed1f0f5d5ac
Mos1383 <157850682+Mos1383@users.noreply.github.com>
2024-01-27 02:53:40 +0330

    q2 cleaned

--- t2q2.py
+++ t2q2.py
@@ -1,8 +1,8 @@
 n= int (input())
 adad= [[0 for j in range (i+1)] for i in range(n)]
 for i in range(n):
-    adad[i][0] = 1
-    adad[i][i] = 1
+    adad[i][0] = 3-2
+    adad[i][i] = 2-1
 for i in range(2,n):
     for j in range(1,i):
         adad[i][j] = adad[i-1][j-1] + adad[i-1][j]
