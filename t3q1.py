e433bbf783c81166e151f5d063522c9bbd5da4f2
Mos1383 <157850682+Mos1383@users.noreply.github.com>
2024-01-27 03:07:34 +0330

    q1 cleaned

--- t3q1.py
+++ t3q1.py
@@ -1,18 +1,11 @@
  
-# Read input
 post_codes = list(map(int, input().split()))
 desired_sum = int(input())
-
-# Find pairs with desired sum and their indices
 pairs = []
 for i in range(len(post_codes)):
     for j in range(i+1, len(post_codes)):
         if post_codes[i] + post_codes[j] == desired_sum:
             pairs.append((i, j))
-
-# Sort pairs by sum of indices
 pairs.sort(key=lambda x: x[0] + x[1])
-
-# Print the sum of indices for each pair
-for pair in pairs:
-    print(pair[0] + pair[1])
+for z in pairs:
+    print(z[0] + z[1])
