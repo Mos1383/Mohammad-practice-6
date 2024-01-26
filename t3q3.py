c44002ce4bf3bfd4f7b1ab8f9ee2299e4bb03a21
Mos1383 <157850682+Mos1383@users.noreply.github.com>
2024-01-27 03:08:37 +0330

    q3 cleaned

--- t3q3.py
+++ t3q3.py
@@ -8,27 +8,9 @@ while n<tedad:
         a=x[e+1:]
         emails.append(a)
         
-        # if a not in emails:
-            # emails.append(a)
-    n+=1
+    n+=.5+.5
 emails=set(emails)                
 s = sorted(emails)       
 for i in s:
     print(i)
 
-# این کد رو جوری تغییر بده که زمان اجراش کمتر بشه
-# tedad=int(input())
-# emails=[]
-
-# for i in range(0,tedad):
-#     emails.append(input())
-# import re
-# z=[]
-# for i in emails:
-#     q= i.index('@')
-#     w= i.index('.com')
-#     e= w-q-1
-#     s = re.findall('^@[A-Za-z]{e}.com')
-#     z.append(s)
-# for i in s:
-#     print(i)        
